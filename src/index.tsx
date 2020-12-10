5ccb4cfa639f573ffca4bc41e8a54e48bb707cd7
梦萦几度 <178533869@qq.com>
2020-12-10 21:08:22 +0800

    配置路径别名

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {test} from '@/utils/index'
+test()
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>test</Text>
+      </View>
+    )
+  }
+}
+
+export default App
---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: 高锐
+ * @Date: 2020-12-10 16:09:01
+ * @LastEditors: 高锐
+ * @LastEditTime: 2020-12-10 16:09:39
+ */
+export function test() {
+  console.log('test')
+}
